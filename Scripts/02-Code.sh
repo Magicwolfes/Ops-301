87bc62d9b4fecb1b9c9e3a3199fd00814fa348b3
Sierra <maldonado.sierra.a@gmail.com>
2023-03-14 09:49:03 -0400

    homework

---
+++ Scripts/02-Code.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+# Sierra Maldonado - worked with Justin H, Genvea & Nick A
+# Import Datetime
+
+
+# Ask what document you want to add date"
+read -p "What document would you like to add today's date to?" document
+
+# Get todays date Y-M-D
+date > document
+
+
+echo "the date has been added"
