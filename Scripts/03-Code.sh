e5fb94c19f47576e1081f910cc38572327479ec1
Sierra <maldonado.sierra.a@gmail.com>
2023-03-15 11:27:27 -0400

    homework

---
+++ Scripts/03-Code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Sierra Maldonado - worked with Justin H, Geneva & Nick A
+https://subscription.packtpub.com/book/networking-and-servers/9781785286216/1/ch01lvl1sec17/working-with-permissions
+https://www.linuxtrainingacademy.com/linux-commands-cheat-sheet/
+
+# File permissions
+
+#main
+
+# Check File permissions
+ls -l Hello.txt
+
+# Change file permissions
+chmod 755 Hello.txt
+
+# end
