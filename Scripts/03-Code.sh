4a74188cd0e4c212d33b5eb494d84043faa274c2
Sierra <maldonado.sierra.a@gmail.com>
2023-03-15 15:35:16 -0400

    homework

--- Scripts/03-Code.sh
+++ Scripts/03-Code.sh
@@ -3,14 +3,24 @@
 https://subscription.packtpub.com/book/networking-and-servers/9781785286216/1/ch01lvl1sec17/working-with-permissions
 https://www.linuxtrainingacademy.com/linux-commands-cheat-sheet/
 
+#!/bin/bash
+# Sierra Maldonado - worked with Justin H, Genvea & Nick A
 # File permissions
 
-#main
+
+#Prompt user for Doc
+read -p "Which Document would you like to edit permissions?" Document
+
+#Prompt user for Command code
+read -p "Type the command code to change the permissons" Code
 
 # Check File permissions
-ls -l Hello.txt
+ls -l "$Document"
 
 # Change file permissions
-chmod 755 Hello.txt
+chmod "$Code" "$Document"
+
+echo "File permissions updated successfully"
+
 
 # end
