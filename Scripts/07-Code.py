8de0d5c4014f19dc32c03480651dfe148d3f95d1
Sierra <maldonado.sierra.a@gmail.com>
2023-03-21 13:12:39 -0400

    homework

---
+++ Scripts/07-Code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Sierra Maldonado - worked with Justin H, Geneva & Nick A
+# Import Directories 
+# https://stackoverflow.com/questions/64455223/convert-a-for-loop-to-a-function-python-and-function-argument-clarification
+
+# Main
+
+import os
+
+### Read user input here into a variable
+
+filename= input("please type your filepath")
+
+# # Declaration of functions
+def chicken():
+    for (root, dirs, files) in os.walk(filename):
+        ### Add a print command here to print ==root==
+        print(f'Root Directory:  + {root}')
+        ### Add a print command here to print ==dirs==
+        print(f'Subdirectory: {dirs}')
+        ### Add a print command here to print ==files==
+        print(f'Files: {files}')
+
+chicken()
+
+
+# End
