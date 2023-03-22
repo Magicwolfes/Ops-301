c292ed00c929438edbfe3de2def9267d90e2a43a
Sierra <maldonado.sierra.a@gmail.com>
2023-03-22 11:38:58 -0400

    homework

---
+++ Scripts/08-code.py
@@ -0,0 +1,22 @@
+#!/usr/bin
+# Sierra Maldonado - worked with Justin H, Geneva & Nick A
+# List
+
+
+
+# main
+
+# Define the list 
+Superlist = ["chicken", "noodle", "soup", "carrot", "steam", "butter", "salt", "tender", "nugget", "corn"]
+
+#print fourth element of list
+print(Superlist[3])
+
+#print the sixth through tenth element
+print(Superlist[5:9])
+
+# Change 
+Superlist[6]=("onion")
+print(Superlist)
+
+# end
