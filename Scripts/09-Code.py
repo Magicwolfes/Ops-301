fde8659ab42d06b535bcb51d45e93f716cf99cdb
Sierra <maldonado.sierra.a@gmail.com>
2023-03-23 11:13:40 -0400

    homework

---
+++ Scripts/09-Code.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Sierra Maldonado - worked with Justin H, Geneva & Nick A
+# Logic
+#23Mar23
+
+# main
+
+
+a = input("Choice a number for choice A!")
+b = input("Choice a number for choice B!")
+c = input("Choice a number for choice C!")
+d = input("Choice a number for choice D!")
+
+
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("A and B are equal")
+else:
+  print("A is greater than B")
+  
+  
+
+if c <= d:
+    print ("C is lesser than /equal to D")
+else:
+    print ("C is greater than / equal to D")
+if d != c:
+    print ("C is not equal to D")
+
+
+#end
