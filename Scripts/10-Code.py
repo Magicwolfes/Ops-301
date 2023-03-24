4fd6bc7629f4a5821603ac64a0d86d3890ed5040
Sierra <maldonado.sierra.a@gmail.com>
2023-03-24 12:12:13 -0400

    homework

--- Scripts/10-Code.py
+++ Scripts/10-Code.py
@@ -5,3 +5,27 @@
 #24Mar23
 
 # main
+
+import os
+
+# Create a new file if it does not exist
+f = open("testfile.txt", "w")
+
+# Append three lines
+f = open("testfile.txt", "a")
+f.write("Today is 24March23 \n")
+f.write("I have a server dog named Zoey \n")
+f.write("She likes long walks")
+
+# Print the first line
+f = open("testfile.txt", "r")
+print(f.read(1))
+
+# Close a file when you're finished
+f = open("testfile.txt", "r")
+print(f.readline())
+
+#Delete the file
+os.remove("testfile.txt")
+
+# End
